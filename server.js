e1a417519546d0d33d96afe95f2bd11aed2d9af9
unknown <sabih1996>
2020-08-04 17:51:09 +0500

    set routes for each section API

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+//connect MONGODB
+connectDB();
+
 app.get("/", (req, res) => res.send("API Running"));
+
+//Define Routes
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
